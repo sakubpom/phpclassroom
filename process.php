2990d0fc7d77e461d6c793a293e3fb95b58503b3
sakubpom <89377876+sakubpom@users.noreply.github.com>
2021-09-20 15:51:34 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,3 +1,27 @@
 <?php
-  echo $_GET['data'];
+    $score = $_GET['data1'];
+    if ($score >= 80) {
+        echo "Your grade is A.";
+    } 
+    else if ($score >= 75) {
+        echo "Your grade is B+.";
+    } 
+    else if ($score >= 70) {
+        echo "Your grade is B.";
+    }
+    else if ($score >= 65) {
+        echo "Your grade is C+.";
+    } 
+    else if ($score >= 60) {
+        echo "Your grade is C.";
+    }
+    else if ($score >= 55) {
+        echo "Your grade is D+.";
+    }
+    else if ($score >= 50) {
+        echo "Your grade is D.";
+    }
+    else {
+        echo "Sorry, you got grade F.";
+    }
 ?>
