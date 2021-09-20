7e62d6a337fc536ae2087a10476b34fab5a229d5
sakubpom <89377876+sakubpom@users.noreply.github.com>
2021-09-20 18:24:38 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,27 +1,16 @@
+
 <?php
-    $score = $_GET['data1'];
-    if ($score >= 80) {
-        echo "Your grade is A.";
-    } 
-    else if ($score >= 75) {
-        echo "Your grade is B+.";
-    } 
-    else if ($score >= 70) {
-        echo "Your grade is B.";
-    }
-    else if ($score >= 65) {
-        echo "Your grade is C+.";
-    } 
-    else if ($score >= 60) {
-        echo "Your grade is C.";
-    }
-    else if ($score >= 55) {
-        echo "Your grade is D+.";
-    }
-    else if ($score >= 50) {
-        echo "Your grade is D.";
-    }
-    else {
-        echo "Sorry, you got grade F.";
-    }
+$score = $_GET['data1'];
+
+if( $score >= 80 ){
+     echo "คุณได้เกรด A";
+}else if( $score >= 70 && $score < 80 ){
+     echo "คุณได้เกรด B";
+}else if( $score >= 60 && $score < 70 ){
+     echo "คุณได้เกรด D";
+}else if( $score >= 50 && $score < 60 ){
+     echo "คุณได้เกรด D";
+}else{
+     echo "คุณสอบตก";
+}
 ?>
