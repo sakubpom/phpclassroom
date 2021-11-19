09d5d1020b4b2231a463aa446c046fd1be1f22fb
sakubpom <89377876+sakubpom@users.noreply.github.com>
2021-11-19 15:50:00 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -2,15 +2,10 @@
   <head>
   </head>
   <body>
-    <table border="0">
+    <table border="1">
       <?php
-        for($i=0;$i<100;$i++){
+        for($i=0;$i<5;$i++){
           echo "<tr>";
-          echo "<td>คอลัมน์</td>";
-          echo "<td>คอลัมน์</td>";
-          echo "<td>คอลัมน์</td>";
-          echo "<td>คอลัมน์</td>";
-          echo "<td>คอลัมน์</td>";
           echo "<td>คอลัมน์</td>";
           echo "</tr>";
         }
