a5a4d2cd0070602c30d034095c79e2e16e2e1542
sakubpom <89377876+sakubpom@users.noreply.github.com>
2021-11-19 15:58:34 +0700

    Create tableloop.php

---
+++ tableloop.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+  </head>
+  <body>
+    table border="1">
+      <tr>
+        <td>คอลัมน์ 1 แถว 1</td>
+        <td>คอลัมน์ 2 แถว 1</td>
+      </tr>
+      <tr>
+        <td>คอลัมน์ 1 แถว 2</td>
+        <td>คอลัมน์ 2 แถว 2</td>
+      </tr>
+    </table>
+  </body>
+</html>
