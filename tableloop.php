a808aa22aaeccf5cfa2e28b5525e91ad5e397e58
sakubpom <89377876+sakubpom@users.noreply.github.com>
2021-11-19 15:59:46 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -2,15 +2,14 @@
   <head>
   </head>
   <body>
-    table border="1">
-      <tr>
-        <td>คอลัมน์ 1 แถว 1</td>
-        <td>คอลัมน์ 2 แถว 1</td>
-      </tr>
-      <tr>
-        <td>คอลัมน์ 1 แถว 2</td>
-        <td>คอลัมน์ 2 แถว 2</td>
-      </tr>
+    <table border="1">
+      <?php
+        for($i=0;$i<5;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์</td>";
+          echo "</tr>";
+        }
+      ?>
     </table>
   </body>
 </html>
