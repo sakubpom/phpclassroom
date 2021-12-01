262d4a379169474830e704f394b22a730b97f29f
sakubpom <89377876+sakubpom@users.noreply.github.com>
2021-12-01 08:46:18 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,11 +1,18 @@
 <?php
-  $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
   $data = json_decode($json);
 
   echo "<table border='1'>";
 
-  foreach ( $data[0] as $key => $val ){
-    echo $key.":".$val."<br>";
+ foreach ( $data[0] as $key => $val ){
+    echo "<tr>";
+    echo "<td>";
+    echo $val."<br>"; 
+    echo "</td>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "</tr>";
   }
 
   echo "</table>";
