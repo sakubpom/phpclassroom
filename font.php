1abad8dd741de6e8af650f1bf3341f6a19f0eddd
sakubpom <89377876+sakubpom@users.noreply.github.com>
2022-02-14 16:34:46 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(shining.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(MiPancakeBold_1.woff);
+        src: url(MiPancakeThin_1.woff);
+        src: url(MiPancake_1.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นางสาว อรปรียา มาลา เลขที่ 17 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นางสาว อรปรียา มาลา เลขที่ 17 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
